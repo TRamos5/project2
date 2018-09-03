06052ad8a2d3143ae03fa3b3ac897c79a9520091
hussein.a.khorshed <hussein.a.khorshed@gmail.com>
2018-09-03 17:20:46 -0500

    hakdo

--- app.js
+++ app.js
@@ -22,18 +22,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.use(require('./routes/tasks'));
-app.use(require('./routes/chat'));
-app.use(require('./routes/create'));
-
 app.use(require('./routes/login'));
 app.use(require('./routes/register'));
 app.use(require('./routes/dashboard'));
 app.use(require('./routes/logout'));
-// app.use(require('./routes/project'));
-// app.use(require('./routes/task'));
-// app.use(require('./routes/subtask'));
+
+app.use(require('./routes/tasks'));
+app.use(require('./routes/chat'));
+app.use(require('./routes/create'));
 app.use(require('./routes/budget'));
+// app.use(require('./routes/project'));
 
 
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,14 +1,35 @@
 let express = require('express');
 let router = express.Router();
+let db = require('./../models');
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/dashboard',function(req,res){
-  
+  router.get('/dashboard', function(req, res){
+   
+    //Declare Varialbes 
+    let today = new Date();
+    
+    //Pulls an array of Project objects 
+    db.project.findAll({
+        where: {
+            user_id: req.user.id
+        }
+    })
+    .then(function(result){
+        var projectName = project.projectName;
+        var owner = project.teamLead;
+        var startDate = project.startDate;
+        var endDate = project.endDate;
 
-    res.render('dashboard', {
-        pageTitle: 'Projects'
+        res.render('/dashboard', {
+            projectName: projectName,
+            owner: owner,
+            startDate: startDate,
+            endDate: endDate
+        })
     })
   })
-
 
 
   
